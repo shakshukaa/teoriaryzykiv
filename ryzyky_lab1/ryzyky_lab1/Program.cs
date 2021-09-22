7fc56cc7b59e6ffb36c9109f626328a9a0ee6956
shakshukaa <81253489+shakshukaa@users.noreply.github.com>
2021-09-22 23:55:47 +0300

    Add files via upload

---
+++ ryzyky_lab1/ryzyky_lab1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ryzyky_lab1
+{
+    class Program
+    {
+        static double chance;
+        static void Main(string[] args)
+        {
+            double homeben, forben;
+            Console.WriteLine("Enter the chance of rain: ");
+      
+            chance = Convert.ToDouble(Console.ReadLine());
+           
+            if(chance < 0 || chance > 1)
+            {
+                Console.WriteLine("Invalid value");
+                Environment.Exit(0);
+            }
+
+            homeben = chance * 2 + (1 - chance) * 5;
+            forben = chance * 0 + (1 - chance) * 8;
+
+            if(homeben > forben)
+            {
+                Console.WriteLine("Sit at home");
+            }
+            else
+            {
+                Console.WriteLine("Go to forest");
+            }
+            Console.ReadKey();
+        }
+    }
+}
